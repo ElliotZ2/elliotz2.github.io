b91ca9a87fb8e7bf1b4f9b10fcbfcead386fd589
Elliot Zou <elliotz2@nycstudents.net>
2022-11-29 18:36:31 -0500

    colors

--- more.js
+++ more.js
@@ -20,7 +20,7 @@ function textColor(backgroundColor) {
     let r = parseInt(hex.substring(0,2), 16);
     let g = parseInt(hex.substring(2,4), 16);
     let b = parseInt(hex.substring(4,6), 16);
-    if ((r*0.299 + g*0.587 + b*0.114) > 150) {
+    if ((r*0.299 + g*0.587 + b*0.114) > 130) {
         return "#000000";
     }
     return "#ffffff";
